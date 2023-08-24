61b3b3e958b2b8eb4435b40514233e1671555fe0
BassGoon <garshion@gmail.com>
2023-08-25 02:51:56 +0900

    Client - 유저 데이터 구현 게임 오브젝트 구현중

---
+++ RedsunMMO/RedsunClient/GameSystem/EObjectType.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedsunClient.GameSystem
+{
+    public enum EObjectType
+    {
+        None = 0,
+
+        Player = 1,
+        Monster = 2,
+        
+        Max,
+    }
+
+    public static class EObjectTypeExtension
+    {
+        public static bool IsValid(this EObjectType type)
+        {
+            switch(type)
+            {
+                case EObjectType.Player:
+                case EObjectType.Monster:
+                    return true;
+                default:
+                    break;
+            }
+            return false;
+        }
+    }
+
+}
--- RedsunMMO/RedsunClient/User/UserData.cs
+++ RedsunMMO/RedsunClient/User/UserData.cs
@@ -14,7 +14,69 @@ namespace RedsunClient.UserData
 
 
 
+        public int MySequence { get; private set; } = 0;
+        public string MyName { get; private set; } = string.Empty;
 
+        public float PositionX { get; private set; } = 0.0f;
+        public float PositionY { get; private set; } = 0.0f;
+
+
+        public float BeforePositionX { get; private set; } = 0.0f;
+        public float BeforePositionY { get; private set; } = 0.0f;
+
+
+        public void Reset()
+        {
+            MySequence = 0;
+            MyName = string.Empty;
+
+            PositionX = PositionY = BeforePositionX = BeforePositionY = 0.0f;
+        }
+
+
+
+
+        // from Packet
+
+        public void OnSCLoginAck(SCLoginAck ack)
+        {
+            MySequence = ack.Seq;
+            MyName = ack.UserName;
+        }
+
+
+        public void OnSCJoinGameAck(SCJoinGameAck ack)
+        {
+            if (null == ack.EnterPosition)
+                return;
+
+            PositionX = ack.EnterPosition.X;
+            PositionY = ack.EnterPosition.Y;
+        }
+
+        public void OnSCMoveAck(SCMoveAck ack)
+        {
+            if (ack.Seq != MySequence)
+                return;
+
+            if (null == ack.MoveTargetPosition)
+                return;
+
+            BeforePositionX = ack.MoveTargetPosition.X;
+            BeforePositionY = ack.MoveTargetPosition.Y;
+        }
+
+        public void OnSCMoveEndAck(SCMoveEndAck ack)
+        {
+            if(ack.Seq != MySequence) 
+                return;
+
+            if (null == ack.MoveEndPosition)
+                return;
+
+            BeforePositionX = ack.MoveEndPosition.X;
+            BeforePositionY = ack.MoveEndPosition.Y;
+        }
 
     }
 }
