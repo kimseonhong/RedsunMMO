47fc0f06dc99f495f1f0538bb7b11bfd1e219dbb
BassGoon <garshion@gmail.com>
2023-08-26 17:28:05 +0900

    Client , rename IGameObject to AGameObject

---
+++ RedsunMMO/RedsunClient/GameSystem/AGameObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedsunClient.GameSystem
+{
+    // Todo : 인터페이스 or abstract 로 구현
+    // Todo : Manager 만들것.
+
+    public abstract class AGameObject
+    {
+        public EObjectType ObjectType { get; init; }
+
+        public AGameObject(EObjectType type)
+        {
+            ObjectType = type;
+        }
+
+
+        //public void SetPosition(float x, float y);
+
+        ////public bool IsInRange
+
+        //public void Update(double elapsedSec);
+    }
+}
